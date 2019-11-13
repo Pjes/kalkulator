462997aeaf03922c9bc1940c844cc26b04d5b1f1
Magda <aleksander.magda@bluebrick.pl>
2019-11-14 00:51:56 +0100

    sp2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,17 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   public banki = [
-    {nazwa: "ING", fix: 5000, perc: 1},
-    {nazwa: "PKO", fix: 0, perc: 2},
-    {nazwa: "Alior", fix: 10000, perc: 0},
-    {nazwa: "Nordea", fix: 0, perc: 3},
-    {nazwa: "mBank", fix: 1000, perc: 2.5},
+    {nazwa: "ING", fix: 5000, perc: 1, show: false},
+    {nazwa: "PKO", fix: 0, perc: 2, show: false},
+    {nazwa: "Alior", fix: 10000, perc: 0, show: false},
+    {nazwa: "Nordea", fix: 0, perc: 3, show: false},
+    {nazwa: "mBank", fix: 1000, perc: 2.5, show: false},
   ]
   constructor() {}
+  public bestBank: any;
   public raty: any[];
   public kwotaKredytu: any = 0;
   public active = false;
-
+  activate(bank){
+    bank.show = !bank.show;
+  }
+  findBest() {
+    this.bestBank = this.banki.reduce((acc, curr) => {
+      acc = this.calculateForBank(acc) > this.calculateForBank(curr) ? curr : acc;
+      return acc;
+    }, this.banki[0]);
+  }
   calculateForBank(bank) {
     return (bank.fix + Number(this.kwotaKredytu) * (1 + bank.perc / 100)).toFixed(2);
   }
@@ -29,13 +38,13 @@ export class HomePage {
   calculate(form) {
     this.active = true;
     const czestotliwosc = form.value.lm === 'l' ? 12 : 1;
-    const N = form.value.okres * czestotliwosc
+    const N = form.value.okres * czestotliwosc;
 
     const a = [...Array(N).keys()].map((num) => num + 1);
     if (form.value.rodzaj === 'a') {
       this.raty = a.map((numerRaty) => {
         const r = form.value.oprocentowanie / 1200;
-        const result = form.value.kwota * (r * (1 + r) ** N) / ( (1 + r) ** N - 1)
+        const result = form.value.kwota * (r * (1 + r) ** N) / ( (1 + r) ** N - 1);
         return result;
       });
       this.kwotaKredytu = this.raty.reduce((prev, curr) => {
@@ -53,5 +62,6 @@ export class HomePage {
       }).toFixed(2);
       this.raty = this.raty.map((rata) => rata.toFixed(2));
     }
+    this.findBest();
   }
 }
